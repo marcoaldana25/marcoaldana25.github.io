5f4927021380cdf496b89bb7b528068fb19eb938
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2021-08-01 15:11:11 -0500

    Initial check in of Animal Mapping Profile Tests

---
+++ AnimalMaintenance/AnimalMaintenance_NetCore/4. Tests/AnimalMaintenance.Managers.Tests/Mapping/AnimalMappingProfileTests.cs
@@ -0,0 +1,30 @@
+﻿namespace AnimalMaintenance.Managers.Tests.Mapping
+{
+    using System.Diagnostics.CodeAnalysis;
+    using AutoMapper;
+    using Managers.Mapping;
+    using Xunit;
+
+    [ExcludeFromCodeCoverage]
+    public class AnimalMappingProfileTests
+    {
+        // Setup local instance of AutoMapper for Unit Testing.
+        private IMapper Mapper
+        {
+            get
+            {
+                var mapperConfiguration = new MapperConfiguration(
+                    config => config.AddProfile<AnimalMappingProfile>());
+
+                return mapperConfiguration
+                    .CreateMapper();
+            }
+        }
+
+        [Fact]
+        public void AnimalMappingProfile_AssertConfigurationIsValid()
+        {
+
+        }
+    }
+}
