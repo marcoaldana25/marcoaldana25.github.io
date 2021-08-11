7140645bb9cbcd93ad11115a359e35add96ecd47
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2021-08-11 14:32:08 -0500

    Updates continued
    Adding method level summaries on interface and adding info on Algorithms

--- AnimalMaintenance/AnimalMaintenance_NetCore/2. Managers/AnimalMaintenance.Managers/IAnimalMaintenanceManager.cs
+++ AnimalMaintenance/AnimalMaintenance_NetCore/2. Managers/AnimalMaintenance.Managers/IAnimalMaintenanceManager.cs
@@ -10,12 +10,29 @@
         /// </summary>
         List<Animal> GetAnimals();
 
+        /// <summary>
+        /// Responsible for returning a single animal from the AnimalMaintenance database with the corresponding Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Animal GetAnimal(int id);
 
+        /// <summary>
+        /// Responsible for adding a new animal record into the AnimalMaintenance database.
+        /// </summary>
+        /// <param name="animalToAdd"></param>
         void AddAnimal(Animal animalToAdd);
 
+        /// <summary>
+        /// Responsible for updating an existing animal record in the Animal Maintenance database.
+        /// </summary>
+        /// <param name="userUpdatedAnimal"></param>
         void UpdateAnimal(Animal userUpdatedAnimal);
 
+        /// <summary>
+        /// Responsible for removing an existing animal record in the Animal Maintenance database.
+        /// </summary>
+        /// <param name="id"></param>
         void DeleteAnimal(int id);
     }
 }
